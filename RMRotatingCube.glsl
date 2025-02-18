b2c9891b6f37c791e176541f0c6cb105ec22c47a
Lucas Jacquemin <lucas.jacquemin2002@gmail.com>
2025-02-18 18:09:18 +0100

    transparent cube

--- RMRotatingCube.glsl
+++ RMRotatingCube.glsl
@@ -9,8 +9,8 @@ uniform float u_time;
 const float maxDist = 100.;
 const float epsilon = 0.01;
 const vec4 bgColor = vec4(0.14, 0.59, 0.73, 1.0);
-const int steps = 200;
-const vec3 lightDir = vec3(0,1,0);
+const int steps = 300;
+const vec3 lightDir = normalize(vec3(1.2, 1, -1.1));
 const vec3 lightColor = vec3(1.0,0.9,0.8);
 const vec3 ambientColor = vec3(0.19, 0.28, 0.37);
 
@@ -19,38 +19,40 @@ float sdfSphere(vec3 pos, vec3 center, float s)
   return length(pos - center) - s;
 }
 
+float sdfBox(vec3 pos, vec3 box)
+{
+  vec3 q = abs(pos) - box;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - .01;
+}
+
 float opSmoothUnion( float d1, float d2, float k )
 {
     float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
     return mix( d2, d1, h ) - k*h*(1.0-h);
 }
 
-float sdfMap(vec3 pos)
+mat2 rot2D(float angle)
 {
-    vec3 s1Center = vec3(cos(u_time + 3.14) * 2. ,sin(u_time + 3.14) * 2.,5);
-    float s1Size = 3.;
-    vec3 s2Center = vec3(cos(u_time) * 2. ,sin(cos(u_time)) * 2.,5);
-    float s2Size = 3.;
-    float s1 = sdfSphere(pos, s1Center, s1Size);
-    float s2 = sdfSphere(pos, s2Center, s2Size);
-
-    float ground = pos.y + 2.0;
-
-    return min(ground, opSmoothUnion(s1, s2, 0.5));
+    float c = cos(angle);
+    float s = sin(angle);
+    return mat2(c, s, -s, c);
 }
 
-// vec3 calculateNormal(vec3 p) {
-//     float epsilon = 0.001;
-//     vec3 ex = vec3(epsilon, 0.0, 0.0);
-//     vec3 ey = vec3(0.0, epsilon, 0.0);
-//     vec3 ez = vec3(0.0, 0.0, epsilon);
+float sdfMap(vec3 pos)
+{
+    vec3 boxSize = vec3(.1);
     
-//     float dx = sdfMap(p + ex) - sdfMap(p - ex);
-//     float dy = sdfMap(p + ey) - sdfMap(p - ey);
-//     float dz = sdfMap(p + ez) - sdfMap(p - ez);
+    pos = mod(pos, 1.0) - .5;
+
+    float ground = pos.y + 0.75;
+
+    // pos += vec3(1.0,0,0);
+    pos.xy *= rot2D(u_time);
+    // pos.xz *= rot2D(u_time);
+    float box = sdfBox(pos, boxSize);
     
-//     return normalize(vec3(dx, dy, dz));
-// }
+    return min(ground, box);
+}
 
 vec3 calculateNormal(vec3 pos)
 {
@@ -63,17 +65,22 @@ vec3 calculateNormal(vec3 pos)
 
 float calculateDiffuse(vec3 pos)
 {
-    float eps = 0.001;
+    float eps = 0.0001;
     return clamp((sdfMap(pos+eps*lightDir)-sdfMap(pos))/eps,0.0,1.0);
 }
 
 void main(){
     vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / u_resolution.y; // [-1; 1]
-    // vec2 uv = (gl_FragCoord.xy / u_resolution.xy) * 2.0 - 1.0; // [-1; 1]
-    // uv.x *= u_resolution.x / u_resolution.y;
+    vec2 mx = (u_mouse.xy * 2.0 - u_resolution.xy) / u_resolution.y;
 
-    vec3 rayOrigin = vec3(0, 0, -3);
-    vec3 rayDir = normalize(vec3(uv, 1));
+    vec3 rayOrigin = vec3(0, 0, -u_time);
+    vec3 rayDir = normalize(vec3(uv, 1.0));
+
+    // rayOrigin.yz *= rot2D(mx.y);
+    // rayDir.yz *= rot2D(mx.y);
+
+    // rayOrigin.xz *= rot2D(mx.x);
+    // rayDir.xz *= rot2D(mx.x);
 
     float m_dist = maxDist;
     float t = 0.0; // total dist
@@ -84,17 +91,15 @@ void main(){
         pos = rayOrigin + rayDir * t;
         m_dist = sdfMap(pos);
 
-        // color = vec3(i) / float(steps);
-
-        if (m_dist > maxDist || m_dist < epsilon) break;
+        if (m_dist > maxDist || m_dist < epsilon) 
+            break;
 
         t += m_dist;
     }
-    color = 1.0 - vec3(t / 15.0);
-    vec3 N = calculateNormal(rayOrigin + rayDir * t);
-    float diffuse = max(dot(N, lightDir), 0.0); // angle between lightDir and the normal
-    diffuse = clamp(diffuse, 0., 1.);
-    diffuse = calculateDiffuse(pos);
+    float diffuse = calculateDiffuse(pos);
+    // vec3 N = calculateNormal(rayOrigin + rayDir * t);
+    // float diffuse = max(dot(N, lightDir), 0.0);
+
     color = lightColor * diffuse + ambientColor;
     // color *= exp( -0.1*t ); // fog 
     gl_FragColor = vec4(color, 1.0);
